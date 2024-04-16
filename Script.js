3e0a428167742e53e3b55d5f5f36d3e4a47420aa
Bigyoan83 <misecur@protonmail.com>
2024-04-16 09:00:00 +0200

    16-04

--- Script.js
+++ Script.js
@@ -3,6 +3,22 @@ var hauteur = 5;
 var plateau = [];
 
 
-function initPlateau(event) {
-    addEventListener("load",event)
+function initPlateau() {
+   var contenuDiv = document.getElementById("contenu");
+   var tableauHTML = document.createElement("table");
+
+   for (var i = 0; i < hauteur; i++); {
+        var ligne = document.createElement("tr");
+        for (var j = 0; j < largeur; j++); {
+            var cellule = document.createElement("td");
+            console.log(cellule);
+            // var img = document.createElement("img");
+            // img.src = "ship.png";   // Chemin de l'image ship.png
+            // cellule.append(img);
+            ligne.appendChild(cellule);
+        }
+        tableauHTML.appendChild(ligne);
+   }
+   contenuDiv.appendChild(tableauHTML);
 }
+window.addEventListener("load", initPlateau);
